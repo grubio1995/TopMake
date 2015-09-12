7dc62b7a993de9e5ebd5438f56eeacbf35c8bb86
Erick Luz <erick.luz4@etec.sp.gov.br>
2015-09-12 14:23:22 -0300

    Inserção do layout de paginas
    Versão com o esboço do layout das paginas

---
+++ Páginas/js/topmake-cosmetics.js
@@ -0,0 +1,13 @@
+function finalizarCarrinho() {
+	alert("Pedido finalizado com sucesso!");
+	document.location.href="carrinho.html";
+}
+
+function finalizarPedido() {
+	alert("Compra finalizada com sucesso!");
+	document.location.href="index.html";
+}
+
+function voltar() {
+	window.history.back();
+}
